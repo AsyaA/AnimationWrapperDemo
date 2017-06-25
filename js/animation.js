4d6dc39796c8b88630a3d2034840eb345cd3eac3
Asya <asya.arzumanyan@iunetworks.am>
2017-06-25 20:20:38 +0400

    almost done, but not finished yet

--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,13 @@
-var percent = 0;
+var animationProgress = 0;
 var paused = true;
-var btn = document.getElementById("playPauseBtn");
+var playPauseBtn = document.getElementById("playPauseBtn");
 var divToAnimate = document.getElementById("box");
 var slider = document.getElementById("animationSlider");
 
-var onePointPercent = 1;
+var onePointPercent = 0;  //shows how percent is the one point change in slider
 
 
-btn.addEventListener('click',function (e) {
+playPauseBtn.onclick = (e) => {
     if(paused) {
         if (!divToAnimate.classList.contains('animate')) {
             divToAnimate.classList.add('animate');
@@ -19,52 +19,52 @@ btn.addEventListener('click',function (e) {
         paused = true;
 
         if(slider.value !=0 && slider.value != 100) {
-            let one =  (100 - percent) /(100 - slider.value);
-            let two =   percent/slider.value ;
+            let one =  (100 - animationProgress) /(100 - slider.value);
+            let two =   animationProgress/slider.value ;
             onePointPercent = one > two ? one : two;
         }
     }
-});
+};
 
 window.setInterval(function () {
     if(!paused) {
-        if(percent < 100)
-            percent++;
-        else percent = 0;
-        console.log(percent);
+        if(animationProgress < 100)
+            animationProgress++;
+        else animationProgress = 0;
+        console.log(animationProgress);
     }
 }, 30);
 
 
 
 
-var oldValue = 75;
+let sliderOldValue = 75;
 slider.addEventListener('input', function () {
    if(paused) {
        //divToAnimate.style.webkitAnimation = 'none';
        document.getElementsByTagName('body')[0].removeChild(divToAnimate);
 
-       let newValue = this.value;
-       let orientation = 0;
-       if(newValue > oldValue) {
-           orientation = 1;
-       } else if(newValue < oldValue) {
-           orientation = -1;
+       let sliderNewValue = this.value;
+       let movingDirection = 0;
+       if(sliderNewValue > sliderOldValue) {
+           movingDirection = 1;
+       } else if(sliderNewValue < sliderOldValue) {
+           movingDirection = -1;
        }
-       oldValue = newValue;
+       sliderOldValue = sliderNewValue;
 
-       orientation *= onePointPercent;
-       percent += orientation;
+       movingDirection *= onePointPercent;
+       animationProgress += movingDirection;
 
-       if(percent < 0) percent = 0;
-       if(percent >= 100) percent = 99.9;
+       if(animationProgress < 0) animationProgress = 0;
+       if(animationProgress >= 100) animationProgress = 99.9;
 
 
 
            create();
            divToAnimate = document.getElementById("box");
            divToAnimate.style.webkitAnimationPlayState = "paused";
-           divToAnimate.style.animationDelay = - 3 * percent / 100  + 's';
+           divToAnimate.style.animationDelay = - 3 * animationProgress / 100  + 's';
 
    }
 });
@@ -75,7 +75,7 @@ function create() {
     div.id = 'box';
     div.innerHTML = "DIV BOX TO ANIMATE";
     div.classList.add('animate');
-    div.style.animationDelay = - 3 * percent / 100  + 's';
+    div.style.animationDelay = - 3 * animationProgress / 100  + 's';
     document.getElementsByTagName('body')[0].appendChild(div);
 }
 
