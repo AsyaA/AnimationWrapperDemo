220e02d1bfda435ec1d76a4c754ac371a48018c3
Asya <asya.arzumanyan@iunetworks.am>
2017-06-25 18:50:34 +0400

    Slider works correct, but a little bit vibrates(vibrates a lot :( )

--- js/animation.js
+++ js/animation.js
@@ -1,47 +1,30 @@
-/**
- * Created by asya on 6/25/17.
- */
-
-
-
-
 var percent = 0;
 var paused = true;
-btn = document.getElementById("playPauseBtn");
-divToAnimate = document.getElementById("box");
+var btn = document.getElementById("playPauseBtn");
+var divToAnimate = document.getElementById("box");
 var slider = document.getElementById("animationSlider");
+
+var onePointPercent = 1, one, two;
+
 
 
 btn.addEventListener('click',function (e) {
     if(paused) {
-        //if (!divToAnimate.classList.contains('animate')) {
+        if (!divToAnimate.classList.contains('animate')) {
             divToAnimate.classList.add('animate');
-        //}
+        }
         divToAnimate.style.webkitAnimationPlayState = "running";
         paused = false;
     } else {
         divToAnimate.style.webkitAnimationPlayState = "paused";
         paused = true;
 
-        // var w = divToAnimate.offsetWidth;
-        // var h = divToAnimate.offsetHeight;
-        // var m_t = divToAnimate.getAttribute('margin-top');
-        // var m_l = divToAnimate.getAttribute('margin-left');
-        // var t = divToAnimate.getAttribute('transform');
-        // var c = divToAnimate.getAttribute('background-color');
-
-       // divToAnimate.classList.remove('animate');
-
-        // console.log(w+"  "+h+"  ");
-        //
-        // divToAnimate.offsetWidth = w;
-        // divToAnimate.offsetHeight = h;
-        // divToAnimate.setAttribute('margin-top', m_t);
-        // divToAnimate.setAttribute('margin-left', m_l);
-        // divToAnimate.setAttribute('transform', t);
-        // divToAnimate.setAttribute('background-color', c);
-
-       // divToAnimate.style.animationDelay = - 3 * percent / 100 - 0.1 + 's';
+        if(slider.value !=0 && slider.value != 100) {
+            one =  (100 - percent) /(100 - slider.value);
+            two =   percent/slider.value ;
+            onePointPercent = one > two ? one : two;
+        }
+        console.log(slider.value + "            " + percent + "      ------------       " + onePointPercent);
     }
 });
 
@@ -50,28 +33,46 @@ var t = window.setInterval(function () {
         if(percent < 100)
             percent++;
         else percent = 0;
-        // slider.value = percent;
-        //console.log(percent);
+        console.log(percent);
     }
 }, 30);
 
+
+
+
+var oldValue = 75;
 slider.addEventListener('input', function () {
     divToAnimate.style.webkitAnimation = 'none';
-    //divToAnimate.classList.remove('animate');
+    divToAnimate.style.animationFillMode = 'forwards';
 
-    divToAnimate.style.webkitAnimation = '';
-    divToAnimate.style.animationDelay = - 3 * slider.value / 100  + 's';
-    //percent = this.value;
-    divToAnimate.style.webkitAnimationPlayState = "paused";
-   // console.log(- 3 * slider.value / 100 + 's');
- //  divToAnimate.classList.add('animate');
-  //  divToAnimate.classList.add('animate')
- //   divToAnimate.classList.add('animate');
+    var newValue = this.value;
+    var orientation = 0;
+
+    if(newValue > oldValue) {
+        orientation = 1;
+    } else if(newValue < oldValue) {
+        orientation = -1;
+    }
+    oldValue = newValue;
+
+
+    orientation *= onePointPercent;
+
+    percent += orientation;
+
+
+    if(percent < 0) percent = 0;
+    if(percent >= 100) percent = 99.9;
+
+
+
+
+    console.log(- 3 * percent / 100)
+    setTimeout(function () {
+        divToAnimate.style.webkitAnimation = '';
+        divToAnimate.style.webkitAnimationPlayState = "paused";
+        divToAnimate.style.animationDelay = - 3 * percent / 100  + 's';
+    }, 1);
+
 
 });
-
-divToAnimate.addEventListener("animationiteration", function (e) {
-    console.log(e.elapsedTime)
-});
-
-
