44ee8a9a96be38bdef26bc7cef8d571f23339f7d
Asya <asya.arzumanyan@iunetworks.am>
2017-06-25 20:05:48 +0400

    everything is ok, just need to clean up in js file

--- js/animation.js
+++ js/animation.js
@@ -4,8 +4,7 @@ var btn = document.getElementById("playPauseBtn");
 var divToAnimate = document.getElementById("box");
 var slider = document.getElementById("animationSlider");
 
-var onePointPercent = 1, one, two;
-
+var onePointPercent = 1;
 
 
 btn.addEventListener('click',function (e) {
@@ -20,15 +19,14 @@ btn.addEventListener('click',function (e) {
         paused = true;
 
         if(slider.value !=0 && slider.value != 100) {
-            one =  (100 - percent) /(100 - slider.value);
-            two =   percent/slider.value ;
+            let one =  (100 - percent) /(100 - slider.value);
+            let two =   percent/slider.value ;
             onePointPercent = one > two ? one : two;
         }
-        console.log(slider.value + "            " + percent + "      ------------       " + onePointPercent);
     }
 });
 
-var t = window.setInterval(function () {
+window.setInterval(function () {
     if(!paused) {
         if(percent < 100)
             percent++;
@@ -42,37 +40,46 @@ var t = window.setInterval(function () {
 
 var oldValue = 75;
 slider.addEventListener('input', function () {
-    divToAnimate.style.webkitAnimation = 'none';
-    divToAnimate.style.animationFillMode = 'forwards';
+   if(paused) {
+       //divToAnimate.style.webkitAnimation = 'none';
+       document.getElementsByTagName('body')[0].removeChild(divToAnimate);
 
-    var newValue = this.value;
-    var orientation = 0;
+       let newValue = this.value;
+       let orientation = 0;
+       if(newValue > oldValue) {
+           orientation = 1;
+       } else if(newValue < oldValue) {
+           orientation = -1;
+       }
+       oldValue = newValue;
 
-    if(newValue > oldValue) {
-        orientation = 1;
-    } else if(newValue < oldValue) {
-        orientation = -1;
-    }
-    oldValue = newValue;
+       orientation *= onePointPercent;
+       percent += orientation;
 
-
-    orientation *= onePointPercent;
-
-    percent += orientation;
-
-
-    if(percent < 0) percent = 0;
-    if(percent >= 100) percent = 99.9;
+       if(percent < 0) percent = 0;
+       if(percent >= 100) percent = 99.9;
 
 
 
+           create();
+           divToAnimate = document.getElementById("box");
+           divToAnimate.style.webkitAnimationPlayState = "paused";
+           divToAnimate.style.animationDelay = - 3 * percent / 100  + 's';
 
-    console.log(- 3 * percent / 100)
-    setTimeout(function () {
-        divToAnimate.style.webkitAnimation = '';
-        divToAnimate.style.webkitAnimationPlayState = "paused";
-        divToAnimate.style.animationDelay = - 3 * percent / 100  + 's';
-    }, 1);
-
-
+   }
 });
+
+
+function create() {
+    let div = document.createElement('div');
+    div.id = 'box';
+    div.innerHTML = "DIV BOX TO ANIMATE";
+    div.classList.add('animate');
+    div.style.animationDelay = - 3 * percent / 100  + 's';
+    document.getElementsByTagName('body')[0].appendChild(div);
+}
+
+
+
+
+
