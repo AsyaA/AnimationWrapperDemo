b08369153a5dbc4814bfaa2a52c0706df1959d7d
Asya <asya.arzumanyan@iunetworks.am>
2017-06-26 09:51:55 +0400

    it's the final commit

--- js/myanimation.js
+++ js/myanimation.js
@@ -1,3 +1,22 @@
+class KeyFrame {
+    constructor(progress, height, width, marginTop, marginLeft, rotateAngle, backgroundColor) {
+        this.progress = progress;
+        this.height = height;
+        this.width = width;
+        this.marginTop = marginTop;
+        this.marginLeft = marginLeft;
+        this.rotateAngle = rotateAngle;
+        this.backgroundColor = backgroundColor;
+    }
+
+    toString () {
+        return `${this.progress}% { height:${this.height}px; width:${this.width}px; margin-top: ${this.marginTop}px;
+         margin-left: ${this.marginLeft}px; transform: rotate(${this.rotateAngle}deg); background-color: ${this.backgroundColor};} \n`;
+    }
+}
+
+
+
 class Animation {
     constructor() {
         this.animationProgress = 0;
@@ -85,6 +104,29 @@ class Animation {
 }
 
 
+
+
+//create keyFrames
+let keyFrames = [];
+keyFrames.push(new KeyFrame(0, 200, 200, 0, 0, 0, 'red'));
+keyFrames.push(new KeyFrame(33, 100, 200, 300, 180, 45, 'lawngreen'));
+keyFrames.push(new KeyFrame(66, 100, 170, 220, 220, 90, 'indigo'));
+keyFrames.push(new KeyFrame(100, 100, 120, 100, 280, 135, 'blue'));
+
+
+//add keyFrames to document
+let styleElement = document.createElement('style');
+styleElement.type = 'text/css';
+let keyFrame = `@keyframes myAnimation { \n `;
+for(let i = 0; i < keyFrames.length; i++) {
+    keyFrame += keyFrames[i].toString();
+}
+keyFrame += '}';
+styleElement.innerHTML = keyFrame;
+document.getElementsByTagName('head')[0].appendChild(styleElement);
+
+
+//setup animation
 let animation = new Animation();
 animation.setListeners();
 animation.calculateProgress();
